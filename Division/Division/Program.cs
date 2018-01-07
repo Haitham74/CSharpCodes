a10b9305f1007113e337d8b359b4a4cce66fffdf
Haitham74 <34990856+Haitham74@users.noreply.github.com>
2018-01-07 04:58:43 -0600

    Division
    Division

---
+++ Division/Division/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 4, b = 3;
+            double d = a / b;
+
+            int myVar1 = 4, myVar2 = 3, myVar;
+            myVar = (myVar1 / myVar2);
+
+            Console.WriteLine($" My division result using DOUBLE is = {d}, \n\nMy division result using INT is = {myVar}");
+            Console.ReadKey();
+        }
+    }
+}
