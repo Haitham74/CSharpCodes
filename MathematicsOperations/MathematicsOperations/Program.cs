946a07d6e1885d1d0384c59e1b24cb798942e5a1
Haitham74 <34990856+Haitham74@users.noreply.github.com>
2018-01-07 05:05:39 -0600

    MathematicsOperations
    Mathematics Operations

---
+++ MathematicsOperations/MathematicsOperations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // + - / * %
+            int myVar1 = 7;
+            int myvar2 = 3;
+
+            // Below are the FOUR simple operations
+            int temp1 = myVar1 + myvar2;
+            int Temp2 = myVar1 - myvar2;
+            int temp3 = myvar2 * myVar1;
+
+            //float flvar1 = myVar1 f/myvar2;
+
+            int temp4 = myVar1 / myvar2;
+            int temp5 = myVar1 % myvar2;
+            int temp6 = temp5 / myvar2;
+            int temp7 = temp4 + temp6;
+
+            Console.WriteLine($"The addtion result is {temp1} \n\nThe subtraction results is {Temp2} \n\nThe multiplication results is {temp3} \n\nThe 7/3 is {temp4} \n\nThe remaining of division is {temp5} \n\nThe remaining divided by 3 is {temp6} \n\nThe division result is {temp7}");
+            Console.ReadKey();
+        }
+    }
+}
