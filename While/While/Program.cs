1b274aac21265fec0dfa06e26ef97a05fe2b7740
Haitham74 <34990856+Haitham74@users.noreply.github.com>
2018-01-07 05:09:39 -0600

    WhileStatement
    While Statement

---
+++ While/While/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            double mySal = 1000.1, total = 0, TOT=0;
+            while (i<10)
+            {
+                total += mySal;
+                i++;
+                TOT += total;
+                Console.WriteLine("Salary number {0} is {1}",i,total);
+            }
+            Console. WriteLine("total is {0}", TOT);
+            Console.ReadKey();
+        }
+    }
+}
