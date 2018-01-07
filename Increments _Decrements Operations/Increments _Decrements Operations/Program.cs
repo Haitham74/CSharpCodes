43b87b9d550aa69c234ff97b4bb2d7cd3a000381
Haitham74 <34990856+Haitham74@users.noreply.github.com>
2018-01-07 05:04:20 -0600

    Increments _Decrements Operations
    Increments _Decrements Operations

---
+++ Increments _Decrements Operations/Increments _Decrements Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increments__Decrements_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 7;
+            int myVar2 = 2;
+            int temp1 = myVar1-- / ++myVar2;
+            //int temp1 = ++myVar1;
+            
+       
+            Console.WriteLine($"{temp1}");
+            Console.ReadKey();
+        }
+    }
+}
