5971962b128a92bd97b2d2e0b7d5cf879309e113
Haitham74 <34990856+Haitham74@users.noreply.github.com>
2018-01-07 05:07:27 -0600

    Nested_If_Statement
    Nested_If_Statement

---
+++ Nested_If_Statement/Nested_If_Statement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_If_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 9, myVar2 = 7;
+
+            if (myVar1 == 9)
+            {
+                Console.WriteLine($"myVar1 equal to 9");
+                if (myVar2 ==6)
+                {
+                    Console.WriteLine($"myVar2 equal to 7");
+                }
+                else
+                {
+                    Console.WriteLine($"myVar2 IS NOT equal to 7");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"myVar1 IS NOT equal to 9");
+            }
+            Console.ReadKey();
+        }
+    }
+}
