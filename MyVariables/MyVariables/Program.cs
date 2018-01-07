5240e400a8bbebb3d03e4474bafee18e3c69a0bf
Haitham74 <34990856+Haitham74@users.noreply.github.com>
2018-01-07 05:06:55 -0600

    MyVariables
    My Variables

---
+++ MyVariables/MyVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int @myValue = 2005;
+            Console.Write(format: "{0}", arg0: @myValue);
+            Console.ReadKey();
+        }
+    }
+}
