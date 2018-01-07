0919b6224e8d4b089b7945d4fd6db0e97caf2288
Haitham74 <34990856+Haitham74@users.noreply.github.com>
2018-01-07 05:04:56 -0600

    Literal_Characters
    Literal_Characters

---
+++ Literal_Characters/Literal_Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Literal_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myvar = 100;
+            uint myUVar = 1000u;            // You can use (U) after the number or you do not need to
+
+            float myflVar = 1.5f;          // You have to use (F) after the number, otherwise it will encounter an error
+            double myDVar = 100.5D;       // You can use (D) after the number or you do not need to
+            decimal myDecVar = 100.5M;    // You have to use (M) after the number, otherwise it will encounter an error
+
+            Console.WriteLine("{0} {1}", myvar, myUVar);
+            Console.WriteLine($"{myflVar} {myDVar} {myDecVar}");
+            Console.ReadKey();
+
+        }
+    }
+}
