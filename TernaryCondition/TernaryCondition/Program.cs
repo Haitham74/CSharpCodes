f105488d7e23691c61f1e1e9b0a1c7f9c2c1f210
Haitham74 <34990856+Haitham74@users.noreply.github.com>
2018-01-09 23:08:06 -0600

    TernaryCondition
    Ternary Condition

---
+++ TernaryCondition/TernaryCondition/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryCondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*=======================USING INTERGER============
+            int myVar = 9;
+            string myResult = (myVar < 8) ? "Less than 8" : "Greater than 8";
+            Console.WriteLine($"my VarResult is {myResult}");
+            */
+           
+            // ====================USING STRING================
+            string myName = "Haitham", result;
+            result = (myName.Equals("Haitham")) ? "YES" : "NO";
+            Console.WriteLine($"my Variable rsult is {result}");
+            Console.ReadKey();
+        }
+        
+    }
+}
