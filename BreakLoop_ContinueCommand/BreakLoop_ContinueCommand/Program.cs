ebaa57482ac82a53c3521b4231ff3629a596b92c
Haitham74 <34990856+Haitham74@users.noreply.github.com>
2018-01-11 08:23:44 -0600

    BreakLoop_ContinueCommand
    Break Loop_Continue Command

---
+++ BreakLoop_ContinueCommand/BreakLoop_ContinueCommand/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakLoop_ContinueCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <=4; i++)
+            {
+                Console.WriteLine($"Hello Haitham");
+                if (i > 2)
+                    continue;  // This will make the internal loop achived AS LONG AS the i < 2. 
+                               // If i become > 2 it will not consider the internal loop and jump to continue the external loop
+                for (int j = 0; j<=2; j++)
+                {
+                    Console.WriteLine($"\t Hello World");
+                }
+                //Console.WriteLine($"Hello Haitham");
+                Console.WriteLine($"\n");
+            }Console.ReadKey();
+        }
+    }
+}
