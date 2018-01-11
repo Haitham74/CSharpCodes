ba6dd6b106232542311ddfd5a82c1d2244c4b3d3
Haitham74 <34990856+Haitham74@users.noreply.github.com>
2018-01-11 07:43:13 -0600

    BreakLoop
    Break Loop

---
+++ BreakLoop/BreakLoop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            for (int i = 0; i <=8; i++)
+            {
+                Console.WriteLine($"Hello Mustafa");
+                if (i >= 3)
+                    break;
+                for (int j = 0; j <= 5; j++)
+                {
+                    Console.WriteLine($"\t Eat Lover");
+                }
+                Console.WriteLine($"\n");
+            }Console.ReadKey();
+            */
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"Multiplication table {i}");
+               // if (i == 7)
+                 //   break;
+                for (int j = 1; j <11; j++)
+                {
+                    Console.WriteLine($"{i} X {j} = {i * j}");
+                } Console.WriteLine($"\n");
+            } Console.ReadKey();
+        }
+    }
+}
