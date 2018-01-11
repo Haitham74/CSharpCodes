87c9e599e00bb03c3bace0d809c3499828ce19fb
Haitham74 <34990856+Haitham74@users.noreply.github.com>
2018-01-11 07:42:12 -0600

    DoWhile
    Do While

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /*
+            double mySalary = 1000.2, total = 0;
+            int i = 0;
+            do
+            {
+                total += mySalary; // total = total + mySalary
+                i++;               // i = i + 1
+                Console.WriteLine($"i = {i} total value is {total}"); // To print no. of month and the accumulated salary.
+
+            } while (i < 10);
+            Console.WriteLine($"TOTAL VALUE IS {total}");
+            */
+
+            double mySalary = 1020;
+            int i = 0;
+            do
+            {
+                mySalary += 20;
+
+                i++;
+
+             if (mySalary == 1980)
+            
+                Console.WriteLine($"At month {i} mySalary is {mySalary}, I will need {i / 12} years and {i % 12} month to reach that Salary");
+            //else
+            //Console.WriteLine($"Not Achieved");
+
+            } while (i < 60);
+            Console.ReadKey();
+            
+
+        }
+
+    }
+}
---
+++ NestedLoop/NestedLoop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* ==============================================General Example==================================
+             for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"They said \"Hello Haitham\"");
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine($"\t I replied \"Hello World\"");
+                } Console.WriteLine($"\n");
+            } Console.ReadKey();
+            */
+
+            /*===============================Multiplication Table from 1 - 5=======================================
+          for (int i = 1; i < 6; i++)
+            {
+              for (int j = 1; j < 11; j++)
+              Console.WriteLine($"{i} X {j} = {i * j}");
+            }Console.ReadKey();
+           */
+
+            /* ==============================Multiplication table from 1 - 6 EVEN Numbers================
+              for (int i = 0; i < 7; i +=2)
+                {
+                  for (int j = 1; j < 11; j++)
+                 Console.WriteLine($"{i} X {j} = {i * j}");
+                 }
+                   Console.ReadKey();
+             */
+
+            // ===============================Multiplication table from 1 - 6 ODD Numbers================
+            for (int i = 1; i < 7; i += 2)
+            {
+                for (int j = 1; j < 11; j++)
+                    Console.WriteLine($"{i} X {j} = {i * j}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
